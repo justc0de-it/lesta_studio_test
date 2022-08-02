d912f7c3e2e71cd1288e35d8ec59e289f6c80f53
bantikgames <anishenko.alex@gmail.com>
2022-08-02 23:13:11 +0500

    Completed the first task

--- 1. number_parity.py
+++ 1. number_parity.py
@@ -1,9 +1,2 @@
 def is_even(value):
-    if value % 2 == 0:
-        print("True")
-    else:
-        print("False")
-
-
-if __name__ == "__main__":
-    is_even(int(input("Введите число: ")))
+    return value & 1 == 0
